3a36687400174b567a475fc7703c35746898fe8a
inganyoyo <inganyoyo@me.com>
2022-09-09 10:17:53 +0900

    MetalLB

--- Metallb/00-install.sh
+++ Metallb/00-install.sh
@@ -1,19 +1,4 @@
-# Metallb
-
-> You can install Metallb
-
----
-
-#### git clone
-
-```
-git clone https://github.com/inganyoyo/kubernetes-yaml.git
-cd ./kubernetes-yaml/Metallb
-```
-
-### Install MetalLB 
+echo "Install MetalLB"
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/metallb.yaml
-```
 kubectl apply -f config.yaml
-```
