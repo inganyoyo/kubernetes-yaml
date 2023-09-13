bb9fb19285bac3e793b1aa1f11015f22acc19f3a
inganyoyo <inganyoyo@me.com>
2023-09-14 06:26:17 +0900

    Update step2_install_worker_node.sh
    1.27

--- Kubernetes-install-helper/scripts/step2_install_worker_node.sh
+++ Kubernetes-install-helper/scripts/step2_install_worker_node.sh
@@ -1,82 +1,50 @@
-echo "https://blog.radwell.codes/2022/07/single-node-kubernetes-cluster-via-kubeadm-on-ubuntu-22-04/"
-echo "##Install general dependencies##"
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install -y apt-transport-https ca-certificates curl
-
-echo "##Install containerd##"
-curl -fsSLo containerd-config.toml \
-  https://gist.githubusercontent.com/oradwell/31ef858de3ca43addef68ff971f459c2/raw/5099df007eb717a11825c3890a0517892fa12dbf/containerd-config.toml
-sudo mkdir /etc/containerd
-sudo mv containerd-config.toml /etc/containerd/config.toml
-
-curl -fsSLo containerd-1.5.13-linux-amd64.tar.gz \
-  https://github.com/containerd/containerd/releases/download/v1.5.13/containerd-1.5.13-linux-amd64.tar.gz
-
-# Extract the binaries
-sudo tar Cxzvf /usr/local containerd-1.5.13-linux-amd64.tar.gz
-
-# Install containerd as a service
-sudo curl -fsSLo /etc/systemd/system/containerd.service \
-  https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
-
-sudo systemctl daemon-reload
-sudo systemctl enable --now containerd
-
-echo "##Install runc##"
-curl -fsSLo runc.amd64 \
-  https://github.com/opencontainers/runc/releases/download/v1.1.3/runc.amd64
-sudo install -m 755 runc.amd64 /usr/local/sbin/runc
-
-echo "##Install CNI network plugins##"
-curl -fsSLo cni-plugins-linux-amd64-v1.1.1.tgz \
-  https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
-sudo mkdir -p /opt/cni/bin
-sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.1.1.tgz
-
-echo "##Forward IPv4 and let iptables see bridged network traffic##"
+# Forwarding IPv4 and letting iptables see bridged traffic
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
 
-sudo modprobe -a overlay br_netfilter
+sudo modprobe overlay
+sudo modprobe br_netfilter
 
-# sysctl params required by setup, params persist across reboots
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
 
-# Apply sysctl params without reboot
 sudo sysctl --system
 
-echo "##Install kubeadm, kubelet & kubectl"
-# Add Kubernetes GPG key
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg \
-  https://packages.cloud.google.com/apt/doc/apt-key.gpg
-
-# Add Kubernetes apt repository
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" \
-  | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-# Fetch package list
 sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 
-version="1.24.4-00"
-echo "Install Version: $version"
-sudo apt-get install -y kubelet=${version} kubeadm=${version} kubectl=${version}
+# Install containerd
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Prevent them from being updated automatically
+sudo apt-get update
+sudo apt-get install containerd.io
+sudo mkdir -p /etc/containerd
+sudo bash -c 'containerd config default > /etc/containerd/config.toml'
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+sudo systemctl enable containerd
+sudo systemctl restart containerd
+
+# Install kubeadm, kubelet and kubectl
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt install -y kubeadm=1.27.1-00 kubelet=1.27.1-00 kubectl=1.27.1-00
 sudo apt-mark hold kubelet kubeadm kubectl
 
-echo "##Ensure swap is disabled##"
-# See if swap is enabled
-swapon --show
-
-# Turn off swap
-sudo swapoff -a
-
-# Disable swap completely
-sudo sed -i -e '/swap/d' /etc/fstab
+sudo systemctl start kubelet
+sudo systemctl enable kubelet
