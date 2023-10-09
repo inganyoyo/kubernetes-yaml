e96e302416bfee2e18a3b72796de7ec828b627e8
inganyoyo <inganyoyo@me.com>
2023-10-09 19:00:41 +0900

    Update step1_install_control_plane.sh

--- Kubernetes-install-helper/scripts/step1_install_control_plane.sh
+++ Kubernetes-install-helper/scripts/step1_install_control_plane.sh
@@ -1,68 +1,86 @@
-# See if swap is enabled
-swapon --show
-# Turn off swap
-sudo swapoff -a
-# Disable swap completely
-sudo sed -i -e '/swap/d' /etc/fstab
+version="1.27.2-00"
 
-# Forwarding IPv4 and letting iptables see bridged traffic
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+sudo apt update && sudo apt -y full-upgrade
+
+sudo apt -y install curl apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt -y install vim git curl wget kubelet=${version} kubeadm=${version} kubectl=${version}
+sudo apt-mark hold kubelet kubeadm kubectl
+
+echo "memory swapoff"
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+sudo swapoff -a
+
+# Enable kernel modules
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Add some settings to sysctl
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+# Reload sysctl
+sudo sysctl --system
+
+
+# Configure persistent loading of modules
+sudo tee /etc/modules-load.d/containerd.conf <<EOF
 overlay
 br_netfilter
 EOF
 
+# Load at runtime
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-iptables  = 1
+# Ensure sysctl params are set
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
-net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
 EOF
 
+# Reload configs
 sudo sysctl --system
 
-sudo apt-get update
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+# Install required packages
+sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
 
-# Install containerd
+# Add Docker repo
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
-sudo apt-get install containerd.io
+# Install containerd
+sudo apt update
+sudo apt install -y containerd.io
+
+# Configure containerd and start service
 sudo mkdir -p /etc/containerd
-sudo bash -c 'containerd config default > /etc/containerd/config.toml'
-sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
-sudo systemctl enable containerd
+sudo containerd config default|sudo tee /etc/containerd/config.toml
+
+# restart containerd
 sudo systemctl restart containerd
+sudo systemctl enable containerd
 
-# Install kubeadm, kubelet and kubectl
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-sudo apt-get update
 
-version="1.27.1-00"
-echo "Install Version: $version"
-sudo apt install -y kubeadm=${version} kubelet=${version} kubectl=${version}
-sudo apt-mark hold kubelet kubeadm kubectl
-
-sudo systemctl start kubelet
 sudo systemctl enable kubelet
 
-# Remove missing `cni-dir` argument from kubelet if present
-sed -i 's/KUBELET_ARGS=.*/KUBELET_ARGS=/' /etc/kubernetes/kubelet.env
+sudo kubeadm config images pull --cri-socket unix:///run/containerd/containerd.sock --kubernetes-version ${version}
 
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16\
+
+sudo sysctl -p
+sudo kubeadm init \
+  --apiserver-advertise-address 10.10.10.100\
+  --pod-network-cidr=10.244.0.0/16\
   --cri-socket unix:///run/containerd/containerd.sock
 
 mkdir -p $HOME/.kube
@@ -78,8 +96,12 @@ exec bash
 
 sleep 5
 
-kubectl taint nodes --all node-role.kubernetes.io/control-plane-
-kubectl taint nodes --all node-role.kubernetes.io/master-
 
-# Install flannel
-kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+curl -O https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
+curl -O https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml
+
+kubectl create -f tigera-operator.yaml
+
+sed -ie 's/192.168.0.0/10.244.0.0/g' custom-resources.yaml
+
+kubectl create -f custom-resources.yaml
