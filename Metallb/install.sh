91addac3bc7cafa6b15067ee734257c75a425753
inganyoyo <inganyoyo@me.com>
2023-10-09 19:02:29 +0900

    Update install.sh

--- Metallb/install.sh
+++ Metallb/install.sh
@@ -1,4 +1,12 @@
 echo "Install MetalLB"
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/metallb.yaml
+
+MetalLB_RTAG=$(curl -s https://api.github.com/repos/metallb/metallb/releases/latest|grep tag_name|cut -d '"' -f 4|sed 's/v//')
+echo $MetalLB_RTAG
+
+mkdir ~/metallb
+cd ~/metallb
+
+wget https://raw.githubusercontent.com/metallb/metallb/v$MetalLB_RTAG/config/manifests/metallb-native.yaml
+
+kubectl apply -f metallb-native.yaml
 kubectl apply -f config.yaml
